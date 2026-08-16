eef4b87157dc2f0b3f6318a16ed75f1151673113
Fran Mesias <franciscojaviermesias@gmail.com>
2026-08-16 18:06:04 +0200

    Semana 1: proyecto Spring Boot configurado con primer endpoint GET /ping

---
+++ src/test/java/com/franmesias/gestordegastos/GestordegastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franmesias.gestordegastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestordegastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
